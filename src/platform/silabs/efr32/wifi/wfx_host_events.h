2a7d928dcf4311b2c2dec4634b9f5a3feae6ebb5
Shrinivas Gutte <Shrinivas.Gutte@silabs.com>
2023-02-17 14:47:50 +0000

    Pull request #582: Adds RS9116 BLE enabled for all applications
    Merge in WMN_TOOLS/matter from feature/rs911x_BLE_bringup__stash_github to RC_1.1.0-1.1
    
    Squashed commit of the following:
    
    commit a225dc89575b04e014491fa7ff5cbd5af186c98e
    Author: bhmanda-silabs <bhavani.manda@silabs.com>
    Date:   Thu Feb 16 19:42:07 2023 +0530
    
        Removed RS91X_BLE_ENABLE from SiWx917_sdk.gni
    
    commit 149dad1663ff5f118feac6de3d74c360f6470067
    Author: shgutte <shgutte@silabs.com>
    Date:   Thu Feb 16 15:06:34 2023 +0530
    
        Added fix for on-off plug application
    
    commit 73f1a2a4aee49a138bb86e9c2259c7d408198f78
    Merge: 7e91288aae 2ede74a5b0
    Author: shgutte <shgutte@silabs.com>
    Date:   Thu Feb 16 15:04:37 2023 +0530
    
        Merge branch 'feature/rs911x_BLE_bringup__stash_github' of https://stash.silabs.com/scm/wmn_tools/matter into feature/rs911x_BLE_bringup__stash_github
    
    ... and 16 more commits

--- src/platform/silabs/efr32/wifi/wfx_host_events.h
+++ src/platform/silabs/efr32/wifi/wfx_host_events.h
@@ -23,7 +23,6 @@
 #include "semphr.h"
 #include "task.h"
 #include "timers.h"
-
 #include "sl_wfx_cmd_api.h"
 #include "sl_wfx_constants.h"
 
@@ -128,6 +127,7 @@ typedef struct __attribute__((__packed__)) sl_wfx_mib_req_s
 #endif /* RS911X_SOCKETS */
 
 #include "sl_status.h"
+#include "stdbool.h"
 
 #ifdef RS911X_WIFI
 #define WLAN_TASK_STACK_SIZE 1024
@@ -138,6 +138,7 @@ typedef struct __attribute__((__packed__)) sl_wfx_mib_req_s
 #else /* WF200 */
 #define WLAN_TASK_STACK_SIZE 1024
 #define WLAN_TASK_PRIORITY 1
+#define BLE_TASK_PRIORITY 1
 #define MAX_JOIN_RETRIES_COUNT 5
 #endif
 
