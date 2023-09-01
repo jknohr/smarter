84a8c1b1738c4fa4b95e624cdd4b7ff62422ea3c
Ezra Hale <ezra.hale@silabs.com>
2023-09-01 09:51:22 -0400

    publish of v2.2.0-1.2-alpha.1 documentation

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.0-1.1',
+    VERSION: '2.2.0-1.2-alpha.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
